5f50e2387ef97577c46bd350ca6e3b5e56504650
Tomasz Grzybowski <tomasz.grzybowski@student.wat.edu.pl>
2018-06-23 10:10:02 +0200

    Create template

---
+++ src/main/java/pl/mgrzybowski/slowka/SlowkaApplication.java
@@ -0,0 +1,12 @@
+package pl.mgrzybowski.slowka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlowkaApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/mgrzybowski/slowka/SlowkaApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.mgrzybowski.slowka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SlowkaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
